998ecd62b29dd68477132dc9c0985ebd96506fcb
Andre <andrepaesbm@gmail.com>
2023-01-23 19:09:26 -0700

    created a function that counts the words of the user via jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-  console.log("This webpage is awesome!");
+$(document).ready(() => {
+  $('#tweet-text').on('input', function() {
+    const len = $(this).val().length;
+    $('#tweet-text, .counter').text(140 - len);
+    if (len > 140) {
+      $('#tweet-text, .counter').addClass('overlimit');
+    } else {
+      if ($('#tweet-text, .counter').hasClass('overlimit')) {
+        $('#tweet-text, .counter').removeClass('overlimit');
+      }
+    }
+  });
 });
