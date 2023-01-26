394b1a140617717ae8efafd90dd5cf93639ff9e1
Andre <andrepaesbm@gmail.com>
2023-01-26 10:46:01 -0700

    Refactor the code, add some message errors and use the escaping method to prevent XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,67 +1,62 @@
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
-const renderTweets = function (tweets) {
-  for (const tweet of tweets) {
-    $('#tweet-container').append(createTweetElement(tweet));
-  }
-  return;
-};
-
-
-const createTweetElement = function (tweet) {
-  let $tweet = $(`<article class="tweet">
-    <header>
-      <span class="user-avatar">
-        <img src=${tweet.user.avatars}>
-      </span>
-      <span class="user-tweet">
-        <h3>${tweet.user.name}</h3>
-      </span>
-      <span class="handle">
-        <h4>${tweet.user.handle}</h4>
-      </span>
-    </header>
-    <p>${tweet.content.text}</p>
-    <footer class="footer-tweet">
-      <span class="footer-date">
-        <time>${tweet.created_at}</time>
-      </span>
-      <span class="footer-icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </span>
-    </footer>
-  </article>`);
-
-  return $tweet;
-};
-
 $(document).ready(function () {
-  renderTweets(tweetData);
+  // Function to create a new tweet element from a tweet object
+  const createTweetElement = function (tweet) {
+    let $tweet = $(`<article class="tweet">
+      <header>
+        <span class="user-avatar">
+          <img src=${tweet.user.avatars}>
+        </span>
+        <span class="user-tweet">
+        <h3>${$("<div>").text(tweet.user.name).html()}</h3>
+        </span>
+        <span class="handle">
+        <h4>${$("<div>").text(tweet.user.handle).html()}</h4>
+        </span>
+      </header>
+      <p>${$("<div>").text(tweet.content.text).html()}</p>
+      <footer class="footer-tweet">
+        <span class="footer-date">
+          <h4>${timeago.format(tweet.created_at)}</h4>
+        </span>
+        <span class="footer-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </span>
+      </footer>
+    </article>`);
+
+    return $tweet;
+  };
+
+  const renderTweets = function (tweets) {
+    $('#tweet-container').empty();
+    for (const tweet of tweets) {
+      $('#tweet-container').prepend(createTweetElement(tweet));
+    }
+  };
+
+  // Function to load all tweets from the server
+  const loadTweets = function () {
+    $.get('/tweets', renderTweets);
+  };
+
+  $(".create-tweet").on('submit', function (e) {
+    e.preventDefault();
+    const tweetText = $(this).find("#tweet-text").val();
+    if (tweetText === "") {
+      return alert("🚫🚫 You must type a tweet! 🚫🚫");
+    } else if (tweetText.length > 140) {
+      return alert("🚫🚫 Your tweet is longer than 140 characters! 🚫🚫");
+    } else {
+      $.ajax({
+        type: 'POST',
+        url: '/tweets/',
+        data: $('.create-tweet').serialize(),
+      })
+        .done(function () {
+          loadTweets();
+        })
+    }
+  });
 });
